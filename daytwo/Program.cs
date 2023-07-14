06d6dd66213c0c0771e9a133d7c296e4456b31f9
Tristan McGinnis <mcginntw@mail.uc.edu>
2023-07-14 14:39:02 -0400

    Created a Fibonacci Sequence function to understand the syntax and basics of C#

---
+++ daytwo/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int fib(int a, int b, int end) {
+    int c = a + b;
+    if (c < end)
+        return fib(b, c, end);
+    Console.WriteLine(c);
+    return(c);
+};
+fib(4, 6, 11);
+
+// int a, b, c, end;
+// while c < end {
+// c = a + b;
+// a = b;
+// b = c;
+// };
+// Console.WriteLine(end);
