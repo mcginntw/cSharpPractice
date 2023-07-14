e689420931df94aadac89f79f51d75afee20f2ca
Tristan McGinnis <mcginntw@mail.uc.edu>
2023-07-14 14:47:58 -0400

    Created a program that takes in a user int and returns that int mirrored (ex: 123 -> 321). Another problem on leetcode for practice.

---
+++ daynine/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace ReverseInt {
+    
+    public class Reverse {
+
+        public static int Flip(int num) {
+            char holdPos;
+            bool negativeFlag = false;
+            string maxInt = "2147483647";
+            int k = 0;
+
+            if (num < Int32.MinValue || num > Int32.MaxValue) {
+                return 0;
+            }
+
+            if (num < 0) {
+                negativeFlag = true;
+                num = num * -1;
+            }
+
+            char[] outarry = Array.ConvertAll(num.ToString().ToArray(), x=>x);
+
+            for (int j = 0; j < outarry.Length / 2; j++) {
+                holdPos = outarry[outarry.Length - j - 1];
+                outarry[outarry.Length - j - 1] = outarry[j];
+                outarry[j] = holdPos;
+
+                // Console.WriteLine(holdPos);
+            }
+
+            string str = new string(outarry);
+            Console.WriteLine(str);
+            if (str.Length == 10) {
+                while (!(str[k] < maxInt[k])) {
+
+                if (str[k] == maxInt[k]) {
+                k++;
+                }
+                if (str[k] > maxInt[k]) {
+                    Console.Beep();
+                    return 0;
+                }
+                
+            }
+            }
+
+            
+            
+            if (str.Length > 10) {
+                return 0;
+            }            
+
+            if (negativeFlag) 
+            return -1 * Convert.ToInt32(str);
+            return Convert.ToInt32(str);
+        }
+
+
+        public static void Main() {
+            Console.WriteLine(Flip(-2147483412));
+        }
+    }
+}
